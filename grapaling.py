02c5938ee645156f71142443678e40179db1a458
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2019-07-19 13:12:03 -0400

    Add some queries exploring entities and relations

--- grapaling.py
+++ grapaling.py
@@ -6,8 +6,40 @@ if __name__ == "__main__":
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)    
     gdb = GraphDatabase("https://grapal.allenai.org:7473/db/data")
 
-    # How many distinct relationship types?
-    q = """MATCH (r:Relation) RETURN DISTINCT r.relation_type"""
+    #
+    # Explore graph metadata
+    #
+    
+    # Node labels:
+    #  Paper
+    #  Venue, Author, Affiliation
+    #  Entity, Relation, RelationInstance
+    q = "CALL db.labels()" 
+    q = "CALL db.schema()" # doesn't work for some reason
+    q = "CALL db.indexes()"
+
+    
+    #
+    # Entity node explorations
+    #
+    # What kind of properties do Entity nodes have?
+    #  entity_id, name
+    q = "MATCH(e:Entity) UNWIND keys(e) AS entity_keys RETURN DISTINCT entity_keys"
+
+    
+    #
+    # Relation node explorations
+    #
+    
+    # What kind of properties do Relation nodes have?
+    #  relation_id, relation_type, relation_subtype
+    q = "MATCH(r:Relation) UNWIND keys(r) AS relation_keys RETURN DISTINCT relation_keys"
+    
+    # How many distinct relationship types? 7
+    q = "MATCH (r:Relation) RETURN DISTINCT r.relation_type"
+
+    # How many distinct relationship subtypes? 560!
+    q = "MATCH (r:Relation) RETURN DISTINCT r.relation_subtype"
     result = gdb.query(q=q, data_contents=True)
-    print(result.rows)
+    print(result.rows)    
     
